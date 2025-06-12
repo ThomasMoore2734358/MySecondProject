290a28bfe5be38e3e8288032455b09b489bd5c8a
Thomas Moore <Moore21745@outlook.com>
2025-06-12 22:01:17 +0300

    qq

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+
+Console.WriteLine("Hello, World!");
+
+// Welcome user to app
+Console.WriteLine();
+
+// Ask for user name
+
+
+// Greet user by name
+
